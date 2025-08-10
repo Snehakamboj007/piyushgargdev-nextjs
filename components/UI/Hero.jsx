cd46b8da6740c90b76c832cc253cce8933f2baed
Sneha Kamboj <snehakamboj008@gmail.com>
2025-08-10 16:21:47 +0530

    Updated Discord button text in Hero.jsx

--- components/UI/Hero.jsx
+++ components/UI/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
                     }}
                     className="block"
                   >
-                    Join Discord Server <BsDiscord />
+                    Join Our Discord 🎉<BsDiscord />
                   </span>
                 </Link>
               </div>
@@ -108,7 +108,7 @@ const Hero = () => {
                     }}
                     className="block"
                   >
-                    Join Discord Server <BsDiscord />
+                    Join Our Discord 🎉 <BsDiscord />
                   </span>
                 </Link>
               </div>
